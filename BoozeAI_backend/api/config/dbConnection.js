a706dfcfb1bfb16b47183a1a3685369b5c656b72
Swayam Debata <swayamdebata2003@gmail.com>
2025-03-08 18:57:38 +0530

    mongo fix #1

--- BoozeAI_backend/api/config/dbConnection.js
+++ BoozeAI_backend/api/config/dbConnection.js
@@ -1,14 +1,23 @@
-const mongoose = require("mongoose")
-const connectDb = async() => {
-    try{
+const mongoose = require("mongoose");
+
+let isConnected = false; // Track connection status
+
+const connectDb = async () => {
+    if (isConnected) {
+        console.log("Using existing database connection");
+        return;
+    }
+
+    try {
         const connect = await mongoose.connect(process.env.MONGO_URI, {
             writeConcern: { w: "majority" }, 
         });
+        isConnected = true; // Mark as connected
         console.log("Database Connected", connect.connection.host, connect.connection.name);
-    }catch(err){
-        console.log(err);
+    } catch (err) {
+        console.error("Database Connection Error:", err);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDb;

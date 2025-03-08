7dbaeeeaa79d443be06cf2056b6cbd40ecb49b59
Swayam Debata <swayamdebata2003@gmail.com>
2025-03-08 14:41:22 +0530

    commit

--- BoozeAI_backend/api/server.js
+++ BoozeAI_backend/api/server.js
@@ -35,5 +35,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
-module.exports = app;
+module.exports = serverless(app);
 module.exports.handler = serverless(app);

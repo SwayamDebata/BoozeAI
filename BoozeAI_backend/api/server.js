2ae7e828c7d02a8fa09c51869ee564d49ecdb4e5
Swayam Debata <swayamdebata2003@gmail.com>
2025-05-11 21:17:57 +0530

    optimization

--- BoozeAI_backend/api/server.js
+++ BoozeAI_backend/api/server.js
@@ -35,4 +35,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
-module.exports = serverless(app);
+module.exports = app;
